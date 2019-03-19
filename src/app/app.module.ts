e7a3a6e0a2a1acc9bc9764f53c5c1fd8d97877c9
george <yuramail222@gmail.com>
2019-03-19 15:33:29 +0200

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstService } from './first.service';
 import { DataComponent } from './data/data.component';
+import {DataService} from './data.service';
+import { WorkerComponent } from './worker/worker.component';
+import {WorkerService} from './worker.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataComponent
+    DataComponent,
+    WorkerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService, WorkerService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
